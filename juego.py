377b7484f33460afe5e7e712011bb57bef598761
peraltamatiaz940-oss <peraltamatiaz940@gmail.com>
2026-04-25 21:08:01 -0300

    Acciones del juego

--- juego.py
+++ juego.py
@@ -5,3 +5,7 @@ print("tenes",vida,"de vida")
 print("1-pelear")
 print("2-curarse")
 opcion=int(input("elegi una opcion"))
+if opcion == 1:
+  print("peleaste")
+elif opcion == 2:
+  print("te curaste")
